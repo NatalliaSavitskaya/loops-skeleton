a26372863ce22f450ab8312c62ad12ef1d21e6c0
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-02-29 20:21:06 +0200

    resolved

--- src/main/java/com/epam/rd/qa/at/Task.java
+++ src/main/java/com/epam/rd/qa/at/Task.java
@@ -3,14 +3,11 @@ package com.epam.rd.qa.at;
 public class Task {
 	
 	public static void main(String[] args) {
-			printSquaresAndCubes(9);
+		int n=9;
+		for (int i = 1; i <= n; i++) {
+			int square = i * i;
+			int cube = i * i * i;
+			System.out.println(i + " " + square + " " + cube);
 		}
-
-		public static void printSquaresAndCubes(int n) {
-			for (int i = 1; i <= n; i++) {
-				int square = i * i;
-				int cube = i * i * i;
-				System.out.println(i + " " + square + " " + cube);
-			}
 		}
 	}
