1573e9f69700f5166214562d36278697857a76cf
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-02-29 20:14:00 +0200

    Solved

--- src/main/java/com/epam/rd/qa/at/Task.java
+++ src/main/java/com/epam/rd/qa/at/Task.java
@@ -3,7 +3,14 @@ package com.epam.rd.qa.at;
 public class Task {
 	
 	public static void main(String[] args) {
-		// PLACE YOUR CODE HERE
-	}
+			printSquaresAndCubes(9);
+		}
 
-}
+		public static void printSquaresAndCubes(int n) {
+			for (int i = 1; i <= n; i++) {
+				int square = i * i;
+				int cube = i * i * i;
+				System.out.println(i + " " + square + " " + cube);
+			}
+		}
+	}
